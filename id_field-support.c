6f187ee9e1edd709befa748ce15e3fcd0d2da04e
Mario Yudhiprawira <myudhiprawira@gmail.com>
2014-11-25 23:07:17 +0700

    Update id_field-support.c

--- id_field-support.c
+++ id_field-support.c
@@ -182,3 +182,55 @@ int tranvbn(Board x, char c[20]) {
 		}
 	return i;
 	}
+
+void init_chance(chances *ch) { // Inisialisasi array kesempatan
+	strcpy((*ch)[1].Type, "Masuk Penjara");
+	strcpy((*ch)[2].Type, "Tiket Bus");
+	strcpy((*ch)[3].Type, "Tukar Gedung");
+	strcpy((*ch)[4].Type, "Mini Game TicTacToe");
+	}
+	
+void chance(chances ch) {
+	// Kamus Lokal
+	int x;
+	// Algoritma
+	x = rand() & 4;
+	switch (x) {
+		case 1	:	// isi dengan fungsi board
+		case 2	:
+		case 3	:
+		case 4	:
+		default	:
+		}
+	}
+	
+void undian(Address *P) {
+	char q[5];
+	char w[2][4];
+	int x,y;
+	bool b,c ;
+	scanf("%s", &q);
+	y = 1;
+	strcpy(w[1] , "Head");
+	strcpy(w[2], "Tail");
+	c = 0;
+	b = 0;
+	do {
+		printf("Taruhan Anda (Head/Tail) : ");
+		scanf("&s", &q);
+		if ((strcmp(q, w[1]) != 1) && (strcmp(q, w[2]) != 1)) {
+			// c = 1;
+			printf("Input salah! Mohon ulangi! (case sensitive)\n");
+			}
+		else {
+			x = rand() % 2;
+			y += 1;
+			if ((strcmp(q, w[1]) != 1) || (strcmp(q, w[2]) != 1)) {
+				b = 1;
+				y = 1;
+				}
+			}
+		}
+	while ((y != 4) && (!(b)));
+	balance(P) *= y;
+	}
