06130367be6634ac9e618c6d033bf058979fbf28
Mario Yudhiprawira <myudhiprawira@gmail.com>
2014-12-04 02:25:35 +0700

    Update Player.c

--- Player.c
+++ Player.c
@@ -63,27 +63,6 @@ void Register(UserTab *T, char c[20]){
 		Printf("Anda Berhasil Mendaftar\n");
 		}
 	}
-
-void Logon(Queue *Q, UserTab T) {
-	// Kamus Lokal
-	address P, Hex;
-	// Algoritma
-	P = Head(*Q);
-	if ((P) == Nil) {
-		(P) = (address)malloc(sizeof(Pemain));
-		if ((*P) != Nil) {
-			Head(*Q) = (P);
-			Tail(*Q) = (P);
-			}
-		}
-	else {
-		P = Tail(*Q);
-		Hex = (address)malloc(sizeof(Pemain));
-		P  = Next(P);
-		P = Hex;
-		Tail(*Q) = P;
-		}
-	}
 	
 void NextTurn(Queue *Q) {
 	Head(*Q) = Next(Head(*Q));
@@ -136,18 +115,32 @@ bool NCmp(UserTab T, char c[20]) {
 	return (b);
 	}
 	
-void Login(Queue *Q, UserTab T, Address P) {
-	// Kamus Lokal
-	char c[20];
+void Login(Queue *Q, UserTab T, Address P, char c[20]) {
 	// Algoritma
-	scanf("%s", &c);
-	if (NCmp(T, c)) {
-		Logon((&(*Q)), T);
-		P = Tail(*Q);
-		init_player(P, c);
+	Logon((&(*Q)), T);
+	P = Tail(*Q);
+	// init_player(P, c);
+	}
+	
+void Logon(Queue *Q, UserTab T) {
+	// Kamus Lokal
+	address X, Hex;
+	// Algoritma
+	P = Head(*Q);
+	if ((P) == Nil) {
+		(P) = (address)malloc(sizeof(Pemain));
+		if ((*P) != Nil) {
+			Head(*Q) = (P);
+			Tail(*Q) = (P);
+			}
 		}
-	else 
-		printf("Nama belum terdaftar. Silakan Register terlebih dahulu\n");
+	else {
+		P = Tail(*Q);
+		Hex = (address)malloc(sizeof(Pemain));
+		P  = Next(P);
+		P = Hex;
+		Tail(*Q) = P;
+		}
 	}
 
 void open(f) {
@@ -188,3 +181,49 @@ void f2arrcpy(UserTab *T) {
 		}
 	*Neff = i;
 	}
+
+void finlist(Queue *Q) {
+	Next(Tail(*Q)) = Head(*Q);
+	}
+	
+void createHSB(ScoreBoard *SB) {
+	// Kamus Lokal
+	int i = 0;
+	// Algoritma
+	for (i; i <= 9; ++i) {
+		strcpy((*SB)[i].NameRec, BlankScore);
+		(*SB)[i].AssetsRec = 0;
+		}
+	}
+
+void AddHS(ScoreBoard *SB, Queue Q) {
+	// Kamus Lokal
+	int i, j;
+	// Algoritma
+	i = 0;
+	j = 9;
+	do {
+		++i;
+		}
+	while (aset(Head(Q)) <= (*SB)[i].AssetsRec);
+	for (j = 9; j < i; --j) {
+		(*SB)[j].AssetsRec = (*SB)[j-1].AssetsRec;
+		strcpy((*SB)[j].AssetsRec, (*SB)[j-1].AssetsRec);
+		}
+	strcpy((*SB)[i].NameRec, Name(Head(Q)));
+	(*SB)[i].AssetsRec = aset(Head(Q));
+	}
+	
+void PrintHS(ScoreBoard SB) {
+	// Kamus Lokal
+	int i = 0;
+	// Algoritma
+	printf("High Score Board\n\n");
+	for (i; i <= 9; ++i) {
+		printf("%d\t%s\t%ld", i, SB[i].NameRec, SB[i].AssetsRec);
+		}
+	}
+	
+// masalah load/save string ke eksternal	
+void loadHS() {} // Masih belum selesai
+void saveHS() {} // sda
