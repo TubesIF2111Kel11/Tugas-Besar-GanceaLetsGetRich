02d80ed3c1ebf84057a5dc6a8885db7a324bc4dd
Mario Yudhiprawira <myudhiprawira@gmail.com>
2014-12-04 02:27:40 +0700

    Create main.c
    File utama yang akan dijalankan pengguna

---
+++ main.c
@@ -0,0 +1,108 @@
+// main file
+
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include "player.h"
+
+UserTab T;
+Queue Q;
+char c[20];
+Address P;
+static FILE *textuser;
+char l;
+int k, y, nump, i;
+ScoreBoard SB;
+
+
+int main() {
+	int i = 0;
+	srand(time(0));
+	open();
+	//f2arrcpy(&a, &Neff);
+	while(fgets(a, 20, text) != NULL) {
+		read();
+		strcpy(((a)[i]), cc);
+		++i;
+		}
+	Neff = i;
+	do {
+		printf("MENU UTAMA\n");
+		printf("1. Register\n");
+		printf("2. Login\n");
+		printf("3. About\n");
+		printf("4. Keluar\n");
+		printf("Pilihan Anda : ");
+		scanf("%d", &k);
+		switch (k) {
+			case 1 :	printf("--Register--\n");
+						do {
+							printf("Masukkan User Name yang ingin digunakan\n");
+							scanf("%s", &c);
+							if (NCmp(T, c))
+								printf("Nama sudah digunakan\n");
+							else
+								Register(&T, c);
+							}
+						while (Ncmp(T, c));
+						break;							
+			case 2 :	printf("--Login--\n");
+						printf("Masukkan User Name\n");
+						scanf("%s", &c);
+						if (NCmp(T, c)) {
+							Login(&Q, T, P, c);
+							printf("Login Berhasil/n");
+							}
+							do {
+								printf("Preparation Menu\n");
+								printf("1. New Game\n");
+								printf("2. Load Game\n");
+								printf("3. High Score Board\n");
+								printf("4. Log Out\n");
+								printf("Pilihan Anda : ");
+								scanf("%d", &y);
+								switch (y) {
+									case 1 :	do {
+													printf("Jumlah Pemain\n");
+													scanf("%d", &nump);
+													if ((nump > 4) || (nump < 2))
+														printf("Jumlah pemain harus kurang dari 2 atau lebih dari 4\n");
+													}
+												while ((nump > 4) || (nump < 2));
+												printf("Nama Pemain 1 : %s\n", Name(Tail(Q)));
+												for (i = 2; i <= nump; ++i) {
+													printf("Nama Pemain %d : ", i);
+													scanf("%s", &c);
+													}
+												finlist(&Q);
+												// diisi oleh bagian board & game
+												break;
+									case 2 :	
+									case 3 :	PrintHS(SB);
+									case 4 :	printf("Anda berhasil Log Out\n\n");
+												break;
+									default :	printf("Input salah! mohon ulangi\n\n");
+												break;
+									}
+								}
+							while ((y != 1) && (y != 2) && (y != 4));
+													
+						printf("\n");
+						break;
+			case 3 :	printf("About the Developers\n\n");
+						// Isi dengan Nama/NIM kelompok
+						
+						break;
+			case 4 :	printf("Selamat Tinggal!\n\n");
+						close();
+						break;
+			default :	printf("Input salah! mohon ulangi\n\n");
+						break;
+			}
+		}
+	while (k != 3);
+	return 0;
+	}
+	
+	
